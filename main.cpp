e5b17ecc634aa5181c6ba9b680594e603732edf7
Lucas Abdalla <lucasdsabdalla@gmail.com>
2024-12-02 21:31:06 -0300

    Working lamp

--- main.cpp
+++ main.cpp
@@ -1,103 +1,132 @@
 #include <GL/glut.h>
-#include <iostream>
+#include <math.h>
 
-// Funções de callback
-void display();
-void reshape(int width, int height);
-void keyboard(unsigned char key, int x, int y);
-void drawArm();
-void drawCube();
-void setupLighting();
+float angle1 = 0.0, angle2 = 0.0, angle3 = 0.0;
 
-int main(int argc, char** argv) {
-    // Inicializa o FreeGLUT
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Modelo 3D Articulado");
+void drawSegment() {
+    glPushMatrix();
+    glColor3f(0.2, 0.2, 0.2); // Set color to dark gray
+    glScalef(1.0, 0.2, 0.2);
+    glutSolidCube(1.0);
+    glPopMatrix();
+}
 
-    // Configurações de callback
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
+void drawSphere(float size, float r, float g, float b) {
+    glColor3f(r, g, b); // Set color
+    glutSolidSphere(size, 20, 20);
+    glColor3f(1.0, 1.0, 1.0); // Reset color to white
+}
 
-    // Configurações de OpenGL
-    glEnable(GL_DEPTH_TEST);
+void drawBase() {
+    glPushMatrix();
+    glColor3f(0.2, 0.2, 0.2); // Set color to dark gray
+    glTranslatef(0.0, -0.25, 0.0); // Adjust position to be closer to the first segment
+    glRotatef(-90, 1.0, 0.0, 0.0);
+    glutSolidCone(0.25, 0.5, 20, 20); // Make the cone smaller
+    glColor3f(1.0, 1.0, 1.0); // Reset color to white
+    glPopMatrix();
+}
 
-    // Loop principal do FreeGLUT
-    glutMainLoop();
-    return 0;
+void drawTable() {
+    glPushMatrix();
+    glColor3f(0.6, 0.3, 0.0); // Set color to brown
+    glTranslatef(0.0, -0.75, 0.0); // Position the table under the base
+    glScalef(10.0, 0.5, 10.0); // Scale the cube to make it look like a table
+    glutSolidCube(1.0);
+    glColor3f(1.0, 1.0, 1.0); // Reset color to white
+    glPopMatrix();
+}
+
+void setupLighting() {
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
+void updateLightPosition() {
+    GLfloat light_position[] = { 1.5 + 0.5 * cos(angle1 * M_PI / 180.0), 0.0, 0.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Configurações da câmera
-    gluLookAt(0.0, 0.0, 5.0,  // Posição da câmera
-              0.0, 0.0, 0.0,  // Ponto para onde a câmera está olhando
-              0.0, 1.0, 0.0); // Vetor "up"
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    // Desenha o modelo 3D articulado
-    drawArm();
+    glTranslatef(-1.5, 0.0, 0.0);
+
+    drawTable(); // Draw the table
+    drawBase(); // Draw the base
+
+    glPushMatrix();
+    glTranslatef(0.0, 0.25, 0.0); // Move the sphere to the top of the cone
+    drawSphere(0.1, 1.0, 0.0, 0.0); // Add a red sphere between the cone and the first segment
+    glRotatef(angle1, 0.0, 0.0, 1.0);
+    glTranslatef(0.5, 0.0, 0.0);
+    drawSegment();
+
+    glTranslatef(0.5, 0.0, 0.0);
+    glRotatef(angle2, 0.0, 0.0, 1.0);
+    drawSphere(0.1, 1.0, 0.0, 0.0); // Add a red sphere
+    glTranslatef(0.5, 0.0, 0.0);
+    drawSegment();
+
+    glTranslatef(0.5, 0.0, 0.0);
+    glRotatef(angle3, 0.0, 0.0, 1.0);
+    drawSphere(0.1, 1.0, 0.0, 0.0); // Add a red sphere
+    glTranslatef(0.5, 0.0, 0.0);
+    drawSegment();
+
+    glTranslatef(0.5, 0.0, 0.0);
+    drawSphere(0.2, 1.0, 1.0, 1.0); // Make the last sphere white and bigger
+    GLfloat light_position[] = { 1.5 + 0.5 * cos(angle1 * M_PI / 180.0), 0.0, 0.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position); // Update the light position to move with the sphere
+    glPopMatrix();
 
     glutSwapBuffers();
 }
 
-void reshape(int width, int height) {
-    glViewport(0, 0, width, height);
+void init() {
+    glClearColor(0.5, 0.5, 0.5, 1.0); // Set clear color to gray
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    setupLighting();
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (double)width / (double)height, 1.0, 100.0);
+    gluPerspective(45.0, 1.0, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    /*if (key == 27) // Tecla ESC
-        glutLeaveMainLoop();*/
+    switch (key) {
+        case 'q': angle1 += 5.0; break;
+        case 'a': angle1 -= 5.0; break;
+        case 'w': angle2 += 5.0; break;
+        case 's': angle2 -= 5.0; break;
+        case 'e': angle3 += 5.0; break;
+        case 'd': angle3 -= 5.0; break;
+    }
+    glutPostRedisplay();
 }
 
-void drawArm() {
-    // Desenha a base do braço
-    glPushMatrix();
-    glTranslatef(0.0f, -0.5f, 0.0f);
-    glScalef(0.2f, 0.5f, 0.2f);
-    drawCube();
-    glPopMatrix();
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Mechanical Arm");
 
-    // Desenha a parte superior do braço
-    glPushMatrix();
-    glTranslatef(0.0f, 0.5f, 0.0f);
-    glRotatef(45.0f, 0.0f, 0.0f, 1.0f); // Rotação da articulação
-    glTranslatef(0.0f, 0.5f, 0.0f);
-    glScalef(0.2f, 0.5f, 0.2f);
-    drawCube();
-    glPopMatrix();
-}
+    init();
 
-void drawCube() {
-    glBegin(GL_QUADS);
-    // Define os vértices do cubo
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    // Repita para as outras faces do cubo
-    glEnd();
-}
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
 
-// Configuração da iluminação
-void setupLighting() {
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat light_diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glutMainLoop();
+    return 0;
 }
